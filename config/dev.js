5f5ebdeed7c66ede130136dfc00f3d7c8eecf813
Олександр Вишняк <cherryCRazY>
2019-01-28 23:33:50 +0200

    start working with mobgoDB

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,6 @@
 module.exports = {
     googleClientID:
         "747312663386-r0fa0rl51qjq5rbrrcvil8p4dk2kkqvm.apps.googleusercontent.com",
-    googleClientSecret: "AEvH9lHC3DmEt1EE0_MZL6xk"
+    googleClientSecret: "AEvH9lHC3DmEt1EE0_MZL6xk",
+    cookieKey: `cookieKey`
 };
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const passport = require("passport");
+const cookieSession = require("cookie-session");
+const keys = require("./config/keys");
 
 require("./services/passport");
 
 app.use(express.json());
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
