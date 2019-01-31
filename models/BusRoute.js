14b8e3d2540e72f78e14cd3f5bdea56253c421c8
cherryCRazY <bebebe12@ukr.net>
2019-01-31 11:47:15 +0200

    BusRoute model was created

---
+++ models/BusRoute.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import Joi from "joi";
+
+const busRouteSchema = new mongoose.Schema({
+    fromCity: {
+        type: String,
+        required: true
+    },
+    toCity: {
+        type: String,
+        required: true
+    },
+    data: {
+        type: Date,
+        required: true
+    }
+});
+
+const BusRoute = mongoose.model("BusRoute".busRouteSchema);
+
+function validateBusRoute(busRoute) {
+    const schema = {
+        toCity: Joi.string().required(),
+        fromCity: Joi.string().required(),
+        data: Joi.date().required()
+    };
+
+    return Joi.validate(busRoute, schema);
+}
+
+module.exports.BusRoute = BusRoute;
+module.exports.validate = validateBusRoute;
